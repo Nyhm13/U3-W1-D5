e369c446411ef1ebefc78cc6c821c05ec1ed9392
Nyhm13 <ryo131995@gmail.com>
2025-04-04 16:50:47 +0200

    Update TrendingNow.jsx

--- src/Components/TrendingNow.jsx
+++ src/Components/TrendingNow.jsx
@@ -1,96 +1,88 @@
 import { Component } from "react";
-import { Container, Row, Col, Button ,Spinner,Alert } from "react-bootstrap";
-
-
+import { Container, Row, Col, Button, Spinner, Alert } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css"; //
 
 const URL = "http://www.omdbapi.com/?apikey=b3987125&s=";
 
 class TrendingNow extends Component {
   state = {
     movies: [],
-    loading:true,
-    error:false,
-    selectedCard:null,
+    loading: true,
+    error: false,
+    selectedCard: null,
   };
 
   handleSelected = () => {
-    this.setState({ selectedCard: !this.state.selectedCard });}
+    this.setState({ selectedCard: !this.state.selectedCard });
+  };
 
-
-getMovies = () => {
+  getMovies = () => {
     fetch(URL + this.props.search)
-    .then((response) => {
+      .then((response) => {
         if (response.ok) {
-            return response.json();
+          return response.json();
         } else {
-            throw new Error("cado malato");
+          throw new Error("cado malato");
         }
-    })
-    .then((data) => {
+      })
+      .then((data) => {
         console.log(data);
-        this.setState({ movies: data.Search,loading:false,error:false });
+        this.setState({ movies: data.Search, loading: false, error: false });
         console.log("NUOVO ARRAY", data.Search);
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
-        this.setState({loading:false,error:true})
-    });
-};
-componentDidMount() {
-    
+        this.setState({ loading: false, error: true });
+      });
+  };
+  componentDidMount() {
     this.getMovies();
-}
-
+  }
 
   render() {
     return (
       <Container fluid className="my-5 px-4">
         <h4 className="text-white mb-4">{this.props.categoria}</h4>
-        {
-            this.state.loading && !this.state.error && (
-                <div className="text-center">
-                <Spinner animation="border" variant="light" />
-                </div>
-            )
-        }
-        {
-            this.state.error && (
-                <div className="text-center w-100">
-                    <Alert variant="danger" className="w-50 mx-auto mt-4">
-                        Abbiamo scontrato un errore nel caricamento dei file.
-                        Contatta la polizia postale per maggiori informazioni
-                        oppure chiama al numero verde 1245412 dove offriamo
-                        uno psicologo per aiutarti a superare questo momento difficile <br />
-                        Non fare come me, non cadere malato!
-
-                    </Alert>
-                </div>
-                
-            )
-        }
+        {this.state.loading && !this.state.error && (
+          <div className="text-center">
+            <Spinner animation="border" variant="light" />
+          </div>
+        )}
+        {this.state.error && (
+          <div className="text-center w-100">
+            <Alert variant="danger" className="w-50 mx-auto mt-4">
+              Abbiamo scontrato un errore nel caricamento dei file. Contatta la
+              polizia postale per maggiori informazioni oppure chiama al numero
+              verde 1245412 dove offriamo uno psicologo per aiutarti a superare
+              questo momento difficile <br />
+              Non fare come me, non cadere malato!
+            </Alert>
+          </div>
+        )}
         <Row className=" row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 row-cols-xlg-6 g-2 gy-2">
           {this.state.movies.slice(0, 6).map((movie) => {
             return (
-              <Col key={movie.imdbID} className="text-center scala"
-                 
-              >
+              <Col key={movie.imdbID} className="text-center scala">
                 <img
                   src={movie.Poster}
                   alt={movie.Title}
                   className="img-fluid  object-fit-fill px-1 rounded-3 scala"
-                  style={{ height: "150px", width:'300px' }}
+                  style={{ height: "150px", width: "300px" }}
                   onClick={this.handleSelected}
                 />
-                {
-                    this.state.selectedCard && (
-                        <div className="text-center mt-4">
-                            <h5 className="text-white">{movie.Title}</h5>
-                            <p className="text-secondary">{movie.Year}</p>
-                        </div>
-                    )
-                }
-                {/* <h5 className="my-3" >{movie.Title}</h5>
-                  <p>{movie.Year}</p> */}
+                {this.state.selectedCard && (
+                  <div className="text-center mt-4">
+                    <h5 className="text-white ">{movie.Title}</h5>
+                    <div className=" d-flex justify-content-around align-items-center mt-2">
+                      <Button variant="success" className="rounded-circle ">
+                        <i className="bi bi-play-fill text-white"></i>{" "}
+                      </Button>
+                      <p className="text-secondary text-center m-0">
+                        {movie.Year}
+                      </p>
+                    </div>
+                  </div>
+                )}
               </Col>
             );
           })}
